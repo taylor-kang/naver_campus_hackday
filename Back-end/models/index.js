1569d2b0499f41e95393f0ea4621aca45b0737e7
devsoopark <sparkm0128@gmail.com>
2019-05-14 18:42:28 +0900

    feature(테이블 연결): user_public 테이블 연결

--- Back-end/models/index.js
+++ Back-end/models/index.js
@@ -10,4 +10,6 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+db.User = require('./user')(sequelize, Sequelize);
+
 module.exports = db;
