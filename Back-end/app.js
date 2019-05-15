4af46901143d78db2c60eaa4dcadbd6d24c4604a
devsoopark <sparkm0128@gmail.com>
2019-05-16 01:28:34 +0900

    feature(ordersRouter 적용): app.js 에 ordersRouter 적용

--- Back-end/app.js
+++ Back-end/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ordersRouter = require('./routes/orders');
 var sequelize = require('./models').sequelize;
 
 var app = express();
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/orders', ordersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
