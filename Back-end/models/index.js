1ac80b57662f0e03ace6cc2460a69b83b979e201
devsoopark <sparkm0128@gmail.com>
2019-05-16 01:15:55 +0900

    feature(relation 정의): table 간의 relation 정의

--- Back-end/models/index.js
+++ Back-end/models/index.js
@@ -16,4 +16,14 @@ db.Item = require('./item')(sequelize, Sequelize);
 db.Order = require('./order')(sequelize, Sequelize);
 db.Seller = require('./seller')(sequelize, Sequelize);
 
+// Define Relation
+db.User.hasMany(db.Order, {foreignKey: 'user_id'});
+db.Seller.hasMany(db.Order, {foreignKey: 'seller_id'});
+db.Order.hasMany(db.Item, {foreignKey: 'order_id'});
+db.Delivery.hasOne(db.Item, {foreignKey: 'delivery_id'});
+db.Order.belongsTo(db.User);   
+db.Order.belongsTo(db.Seller);
+db.Item.belongsTo(db.Order);
+db.Item.belongsTo(db.Delivery);
+
 module.exports = db;
